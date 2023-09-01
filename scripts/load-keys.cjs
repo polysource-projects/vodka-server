0d331cbda1059a25c280c3a71dc691d588bc8a32
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2023-09-01 11:26:15 +0200

    fix script

--- scripts/load-keys.cjs
+++ scripts/load-keys.cjs
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
+const path = require("path");
 
 const publicKey = process.env.JWT_PUBLIC_KEY;
 const privateKey = process.env.JWT_PRIVATE_KEY;
@@ -8,5 +9,7 @@ if (!publicKey || !privateKey) {
 	throw new Error("Missing JWT keys");
 }
 
-fs.writeFileSync("./keys/vodka.key.pub", publicKey);
-fs.writeFileSync("./keys/vodka.key", privateKey);
+fs.writeFileSync(path.join(__dirname, "../keys/vodka.key"), privateKey);
+
+fs.writeFileSync(path.join(__dirname, "../keys/vodka.key.pub"), publicKey);
+fs.writeFileSync(path.join(__dirname, "../public/vodka.key.pub"), publicKey);
